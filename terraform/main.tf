d4ab443433b98e6336fdd7f5a8785c2255a75395
thecodemountains <thecodemountains@gmail.com>
2024-01-05 18:16:26 -0600

    env var for password

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,20 +9,30 @@ terraform {
 }
 provider "vmworkstation" {
   # Configuration for the provider (if required)
-  # user     = "your_username"
-  # password = "your_password"
-  # url      = "your_vmworkstation_url"
-  https = false
-  debug = true
+  user     = "sysadmin"
+  password = var.VMWORKSTATION_PASSWORD
+  url      = "http://localhost:8697/"
+  https    = false
+  debug    = true
 }
 
-resource "vmworkstation_vm" "ubuntu_vm" {
-  sourceid     = "source_id_of_ubuntu_22_04_template" # Replace with the actual source ID
-  denomination = "Ubuntu22.04LTS-VM"
-  description  = "Ubuntu 22.04 LTS Virtual Machine"
-  path         = "D:\\vmware\\tf\\ub22" # Replace with the actual path
-  processors   = 2                      # Number of processors
-  memory       = 4096                   # Memory in MB
+# data "vmworkstation" "cos7" {
+#   id           = MQLV6NHQI38K3DMFTDBGDAUK2C8HTSJ0
+#   path         = "D:\\vmware\\cos7\\cos7.vmx"
+#   denomination = cos7
+# }
+
+data "vmworkstation_folder" "cos7_folder" {
+  path = "D:\\vmware\\cos7\\cos7.vmx"
 }
 
-# Add other configurations if necessary
+# resource "vmworkstation_vm" "ubuntu_vm" {
+#   sourceid     = "tf-ub22" # Replace with the actual source ID
+#   denomination = "Ubuntu22.04LTS-VM"
+#   description  = "Ubuntu 22.04 LTS Virtual Machine"
+#   path         = "D:\\vmware\\tf\\ub22" # Replace with the actual path
+#   # path       = "D:\\vmware\\ub18c1\\ub18c1.vmx"
+#   processors = 2    # Number of processors
+#   memory     = 6208 # Memory in MB
+# }
+
--- vmworkstation/data_source_vmworkstation_folder.go
+++ vmworkstation/data_source_vmworkstation_folder.go
@@ -1,6 +1,9 @@
 package vmworkstation
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
@@ -13,10 +16,49 @@ func datasourceVMWSFolder() *schema.Resource {
 				Description: "The absolute path of the folder.",
 				Required:    true,
 			},
+			"folder_size": {
+				Type:        schema.TypeInt,
+				Description: "The size of the folder",
+				Computed:    true,
+			},
+			"num_files": {
+				Type:        schema.TypeInt,
+				Description: "The number of files in this folder",
+				Computed:    true,
+			},
 		},
 	}
 }
 
 func datasourceVMWSFolderRead(d *schema.ResourceData, meta interface{}) error {
+	path := d.Get("path").(string)
+
+	// Initialize folder size and file count
+	var folderSize int64
+	var numFiles int
+
+	// Walk through the directory and sum the size of files
+	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			folderSize += info.Size()
+			numFiles++
+		}
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	// Set the data in the Terraform resource data
+	d.Set("folder_size", folderSize)
+	d.Set("num_files", numFiles)
+
+	// Use the path as the ID for this data source
+	d.SetId(path)
+
 	return nil
 }
